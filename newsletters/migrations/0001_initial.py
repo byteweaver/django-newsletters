5dd6b4fb407243da4cab1a6fc9fee00a39f61e22
Richard <richard@devisio.net>
2015-06-30 01:59:54 +0200

    create initial migration for model

---
+++ newsletters/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Subscription',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('email', models.EmailField(max_length=254, unique=True)),
+            ],
+        ),
+    ]
