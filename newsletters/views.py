53d50693a2e6ccf5a7afa67f4878660c107e4e60
Richard <richard@devisio.net>
2015-06-30 09:55:25 +0200

    override create function in api to inject ip automatically

--- newsletters/views.py
+++ newsletters/views.py
@@ -5,11 +5,16 @@ from rest_framework import viewsets
 from newsletters.models import Subscription
 from newsletters.forms import SubscriptionForm
 from newsletters.serializers import SubscriptionSerializer
+from newsletters.utils import get_client_ip
 
 
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+
+    def create(self, request, *args, **kwargs):
+        request.data['ip'] = get_client_ip(request)
+        return super(SubscriptionViewSet, self).create(request, args, kwargs)
 
 
 class SubscriptionCreateView(CreateView):
