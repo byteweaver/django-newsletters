6bf825daca27212b0fe4745e10c863dbfc092ae9
Richard <richard@devisio.net>
2015-06-30 09:20:32 +0200

    add view set for subscription model

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import CreateView, DeleteView
 
+from rest_framework import viewsets
+
 from newsletters.models import Subscription
 from newsletters.forms import SubscriptionForm
+from newsletters.serializers import SubscriptionSerializer
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
 
 
 class SubscriptionCreateView(CreateView):
