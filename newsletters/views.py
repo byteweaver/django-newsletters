a39c14997803b99841ee74b54d38fd4da1186c82
Richard <richard@devisio.net>
2015-06-30 10:00:06 +0200

    move rest framework related view to own file

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,22 +1,7 @@
 from django.views.generic import CreateView, DeleteView
 
-from rest_framework import viewsets
-
 from newsletters.models import Subscription
 from newsletters.forms import SubscriptionForm
-from newsletters.permissions import IsAuthenticatedOrCreateOnly
-from newsletters.serializers import SubscriptionSerializer
-from newsletters.utils import get_client_ip
-
-
-class SubscriptionViewSet(viewsets.ModelViewSet):
-    queryset = Subscription.objects.all()
-    serializer_class = SubscriptionSerializer
-    permission_classes = (IsAuthenticatedOrCreateOnly,)
-
-    def create(self, request, *args, **kwargs):
-        request.data['ip'] = get_client_ip(request)
-        return super(SubscriptionViewSet, self).create(request, args, kwargs)
 
 
 class SubscriptionCreateView(CreateView):
