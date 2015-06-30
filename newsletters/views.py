421f1865831a24438d480ef730046cae5235a71c
Richard <richard@devisio.net>
2015-06-30 09:56:48 +0200

    implement is authenticated or create only permission and use it for api

--- newsletters/views.py
+++ newsletters/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 
 from newsletters.models import Subscription
 from newsletters.forms import SubscriptionForm
+from newsletters.permissions import IsAuthenticatedOrCreateOnly
 from newsletters.serializers import SubscriptionSerializer
 from newsletters.utils import get_client_ip
 
@@ -11,6 +12,7 @@ from newsletters.utils import get_client_ip
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+    permission_classes = (IsAuthenticatedOrCreateOnly,)
 
     def create(self, request, *args, **kwargs):
         request.data['ip'] = get_client_ip(request)
