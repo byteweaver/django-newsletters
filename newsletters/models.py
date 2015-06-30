bc5ba6c49e4c1edd78c19d8f8e25b15a38501ed4
Richard <richard@devisio.net>
2015-06-30 02:03:52 +0200

    add translatable verbose names to model

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,8 +1,13 @@
+from django.utils.translation import ugettext as _
 from django.db import models
 
 
 class Subscription(models.Model):
     email = models.EmailField(unique=True)
+
+    class Meta:
+        verbose_name = _('Subscription')
+        verbose_name_plural = _('Subscriptions')
 
     def __str__(self):
         return self.email
